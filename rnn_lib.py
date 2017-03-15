a97209fa429e2e5e7d40d41a083b98b263b5385d
Lobacheva Ekaterina <lobacheva.tjulja@gmail.com>
2017-03-15 19:24:24 +0300

    Add gitignore, float64 -> float32.

--- rnn_lib.py
+++ rnn_lib.py
@@ -6,27 +6,25 @@ import lasagne
 ### BE CAREFUL WITH DIVISION IN PYTHON.2-3 ###
 
 def init_normalized(n):
-    A = np.random.normal(size=(n, n))
+    A = np.array(np.random.normal(size=(n, n)),dtype = 'float32')
     A = np.triu(A, k = 1) + np.triu(A).T
-    eig, _ = np.linalg.eig(A + np.eye(n))
+    eig, _ = np.linalg.eig(A + np.eye(n,dtype='float32'))
     e = max(eig)
-    W = (A + np.eye(n)) / e
+    W = (A + np.eye(n,dtype='float32')) / e
     return W
     
-
 def init_posdef_w(n):
     # A = 1/N R^T * R, wehere R is standard normal
     # A is positive definite
     # W = (A + I) / max(spec(A + I))
     # spec(W) = {1, l_i < 1}
     
-    R = np.random.normal(size=(n, n))
+    R = np.array(np.random.normal(size=(n, n)),dtype = 'float32')
     A = np.dot(R.T, R) / n
-    eig, _ = np.linalg.eig(A + np.eye(n))
+    eig, _ = np.linalg.eig(A + np.eye(n,dtype='float32'))
     e = max(eig)
-    W = (A + np.eye(n)) / e
+    W = (A + np.eye(n,dtype='float32')) / e
     return W
-
 
 def build_network(inp, seq_len, dim, nhidden, nout, nonlinear, rnn_type):
     l_in = lasagne.layers.InputLayer(shape=(None, seq_len, dim), input_var=inp)
@@ -38,7 +36,7 @@ def build_network(inp, seq_len, dim, nhidden, nout, nonlinear, rnn_type):
     elif rnn_type == 'nrnn':
         hid_to_hid = init_normalized(nhidden)
     elif rnn_type == 'irnn':
-        hid_to_hid = np.eye(nhidden)
+        hid_to_hid = np.eye(nhidden, dtype='float32')
     elif rnn_type == 'nprnn':
         hid_to_hid = init_posdef_w(nhidden)
     else:
